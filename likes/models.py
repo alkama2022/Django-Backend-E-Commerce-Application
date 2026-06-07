3c7097811d9ed2f33bd7a02048c1f4e099218a61
Alkama Umar Liman <alkalineumarliman@gmail.com>
2026-06-07 17:23:29 +0100

    creating and applying migrations

--- likes/models.py
+++ likes/models.py
@@ -10,3 +10,4 @@ class Like(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+    

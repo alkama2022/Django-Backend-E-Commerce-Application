c7a7c1d23f0f55aaac624ed2baadcb6d148ee5aa
Alkama Umar Liman <alkalineumarliman@gmail.com>
2026-06-07 15:38:48 +0100

    creating models classess Product and Customer to Store APP,

--- store/models.py
+++ store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"

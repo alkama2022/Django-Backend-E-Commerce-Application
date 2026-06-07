f5ea011a62e3a2c444ca9c7586fb82ebe2bf6822
Alkama Umar Liman <alkalineumarliman@gmail.com>
2026-06-07 18:56:15 +0100

    views.py

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from store.models import Product
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})

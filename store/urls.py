d9c12dcc3377a061059b9fd217437cad76b19545
Alkama Umar Liman <alkalineumarliman@gmail.com>
2026-06-13 15:01:49 +0100

    ModelViewSet To Help Us to not repeat ourseves

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from store import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:pk>/', views.ProductDetails.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetails.as_view(), name='collection-details'),
     path('collections/', views.CollectionList.as_view(), name='collection-list'),
-    path('collections/<int:pk>/', views.CollectionDetails.as_view()),
+    path('collections/<int:pk>/', views.CollectionDetails.as_view(), name='collection-details'),
+    
 ]
--- store/views.py
+++ store/views.py
@@ -1,37 +1,26 @@
-from urllib import request
 
 from django.db.models.aggregates import Count
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from store.serializers import ProductSerializer,CollectionSerializers
 from .models import Product,Collection
-# Create your views here.
 
-# Create your views here.
-
-class ProductList(ListCreateAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
     def delete(self,request,pk):
         if Product.objects.filter(collection_id=pk).count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_400_BAD_REQUEST)
-    
-class ProductDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('product')).all()
     serializer_class = CollectionSerializers
-
-
-class CollectionDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('product')).all()
-    serializer_class = CollectionSerializers
-    
+    def get_serializer_context(self):
+        return {'request': self.request}
     def delete(self,request,pk):
         if Product.objects.filter(collection_id=pk).count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_400_BAD_REQUEST)

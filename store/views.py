31e55955612f313788d10c9cb675dfa4387d3dd1
Alkama Umar Liman <alkalineumarliman@gmail.com>
2026-06-28 10:17:10 +0100

    Searching And Soting Data

--- store/views.py
+++ store/views.py
@@ -1,22 +1,27 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django.db.models import Count
+from .filters import ProductFilter
 from . import models
 from . import serializers
 
 class ProductViewSet(ModelViewSet):
   queryset = models.Product.objects.all()
-  filter_backends = [DjangoFilterBackend]
-  filterset_fields = ['collection_id' , 'price']
+  filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+  filterset_class = ProductFilter
   serializer_class = serializers.ProductSerilizer  
+  search_fields = ['title','description']
+  ordering_fields = ['price','last_update']
   
   def get_serializer_context(self):
      return {'request' : self.request}
    
   def destroy(self, request, *args, **kwargs):
     if models.OrderItem.objects.filter(product_id = kwargs['pk']).count() > 0:
-      return Response({"Error" : "Product Cannot be deleted becouse it is associated with order items"},status=405)
+      return Response({"Error" : "Product Cannot be deleted becouse it is associated with order items"},status=status.HTTP_405_METHOD_NOT_ALLOWED)
     return super().destroy(request, *args, **kwargs)
   
 class CollectionViewSet(ModelViewSet):
@@ -28,7 +33,7 @@ class CollectionViewSet(ModelViewSet):
    
   def destroy(self, request, *args, **kwargs):
     if models.Product.objects.filter(collection_id = kwargs['pk']).count() > 0:
-      return Response({"Error" : "Collection Cannot be deleted becouse it is associated with order Product"},status=405)
+      return Response({"Error" : "Collection Cannot be deleted becouse it is associated with order Product"},status=status.HTTP_405_METHOD_NOT_ALLOWED)
     return super().destroy(request, *args, **kwargs)
 
 class ReviewViewSet(ModelViewSet):
